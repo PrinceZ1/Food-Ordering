85b742305c820372da8fb68ddb6ccfb3ab1f1429
NguyenDepTrai <nguyen.tt1703@gmail.com>
2025-03-31 08:41:42 +0700

    add detail layout + add domain

---
+++ app/src/main/java/com/princez1/foodapp/activity/DetailActivity.java
@@ -0,0 +1,49 @@
+package com.princez1.foodapp.activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.princez1.foodapp.R;
+import com.princez1.foodapp.databinding.ActivityDetailBinding;
+import com.princez1.foodapp.domain.Foods;
+
+public class DetailActivity extends BaseActivity {
+
+    ActivityDetailBinding binding;
+    private Foods object;
+    private int num=1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getWindow().setStatusBarColor(getResources().getColor(R.color.black));
+
+        getIntentExtra();
+//        setVariable();
+    }
+
+//    private void setVariable() {
+//        binding.backBtn.setOnClickListener(v -> finish());
+//
+//        Glide.with(DetailActivity.this)
+//                .load(object.getImagePath())
+//                .into(binding.pic);
+//        binding.priceTxt.setText("$" + object.getPrice());
+//        binding.titleTxt.setText(object.getTitle());
+//        binding.descriptionTxt.setText(object.getDescription());
+//        binding.rateTxt.setText(object.getStar() + "Rating");
+//        binding.ratingBar.setRating((float) object.getStar());
+//        binding.totalTxt.setText((num*object.getPrice()+"$"));
+//    }
+
+    private void getIntentExtra() {
+        object = (Foods) getIntent().getSerializableExtra("object");
+    }
+}

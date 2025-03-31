2cade3dba2d33b65d6b419f92ab2b53f22673033
NguyenDepTrai <nguyen.tt1703@gmail.com>
2025-03-31 08:24:39 +0700

    first commit

---
+++ app/src/main/java/com/princez1/foodapp/activity/BaseActivity.java
@@ -0,0 +1,22 @@
+package com.princez1.foodapp.activity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.princez1.foodapp.R;
+
+
+public class BaseActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        database = FirebaseDatabase.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        getWindow().setStatusBarColor(getResources().getColor(R.color.white));
+    }
+}

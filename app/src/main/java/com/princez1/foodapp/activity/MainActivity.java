03c325bf157f79a0716484fc91e81ab94135ca75
NguyenDepTrai <nguyen.tt1703@gmail.com>
2025-05-04 14:51:54 +0700

    updates

--- app/src/main/java/com/princez1/foodapp/activity/MainActivity.java
+++ app/src/main/java/com/princez1/foodapp/activity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.princez1.foodapp.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,19 +10,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.Firebase;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
 import com.princez1.foodapp.R;
+import com.princez1.foodapp.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends BaseActivity {
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        
+        setVariable();
+    }
+
+    private void setVariable() {
+        binding.logoutBtn.setOnClickListener(v -> {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
         });
     }
 }
--- app/src/main/java/com/princez1/foodapp/adapter/FoodListAdapter.java
+++ app/src/main/java/com/princez1/foodapp/adapter/FoodListAdapter.java
@@ -1,4 +1,69 @@
 package com.princez1.foodapp.adapter;
 
-public class FoodListAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.RoundedCorner;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.annotations.NotNull;
+import com.princez1.foodapp.R;
+import com.princez1.foodapp.domain.Foods;
+
+import java.util.ArrayList;
+
+public class FoodListAdapter extends RecyclerView.Adapter<FoodListAdapter.viewholder> {
+    ArrayList<Foods> items;
+    Context context;
+
+    public FoodListAdapter(ArrayList<Foods> items){
+        this.items = items;
+    }
+
+
+    @NonNull
+    @Override
+    public FoodListAdapter.viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        View inflate = LayoutInflater.from(context).inflate(R.layout.viewholder_list_food,parent,false);
+        return new viewholder(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FoodListAdapter.viewholder holder, int position) {
+        holder.titleTxt.setText(items.get(position).getTitle());
+        holder.priceTxt.setText("$" + items.get(position).getPrice());
+        holder.timeTxt.setText(items.get(position).getTimeValue() + "min");
+        holder.rateTxt.setText("" + items.get(position).getStar());
+
+//        Glide.with(context)
+//                .load(items.get(position).getImagePath())
+//                .transform(new CenterCrop(), new RoundedCorner(30))
+//                .into(holder.pic);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class viewholder extends RecyclerView.ViewHolder{
+        TextView titleTxt, priceTxt, rateTxt, timeTxt;
+        ImageView pic;
+        public viewholder(@NotNull View itemView){
+            super(itemView);
+
+            titleTxt = itemView.findViewById(R.id.titleTxt);
+            priceTxt = itemView.findViewById(R.id.priceTxt);
+            rateTxt = itemView.findViewById(R.id.rateTxt);
+            timeTxt = itemView.findViewById(R.id.timeTxt);
+            pic = itemView.findViewById(R.id.img);
+        }
+    }
 }

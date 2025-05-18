95036c4d939751b33f10724cdd05bc40a66818aa
Hoang Tran Duy <duyvipk36@gmail.com>
2025-05-18 23:51:09 +0700

    Add Dashboard FE-BE& fix Color

---
+++ app/src/main/java/com/princez1/foodapp/adapter/BestFoodsAdapter.java
@@ -0,0 +1,66 @@
+package com.princez1.foodapp.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.princez1.foodapp.R;
+import com.princez1.foodapp.domain.Foods;
+
+import java.util.ArrayList;
+
+public class BestFoodsAdapter extends RecyclerView.Adapter<BestFoodsAdapter.viewholder> {
+    ArrayList<Foods> items;
+    Context context;
+    public BestFoodsAdapter(ArrayList<Foods> items){
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public BestFoodsAdapter.viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_best_deal,parent,false);
+        return new viewholder(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BestFoodsAdapter.viewholder holder, int position) {
+        holder.titleTxt.setText(items.get(position).getTitle());
+        holder.starTxt.setText(""+items.get(position).getStar());
+        holder.timeTxt.setText(items.get(position).getTimeValue()+" min");
+        holder.priceTxt.setText("$"+items.get(position).getPrice());
+
+        Glide.with(context)
+                .load(items.get(position).getImagePath())
+                .transform(new CenterCrop(), new RoundedCorners(30))
+                .into(holder.pic);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class viewholder extends RecyclerView.ViewHolder{
+        TextView titleTxt,starTxt,timeTxt,priceTxt;
+        ImageView pic;
+        public viewholder(@NonNull View itemView) {
+            super(itemView);
+            titleTxt = itemView.findViewById(R.id.titleTxt);
+            starTxt = itemView.findViewById(R.id.starTxt);
+            timeTxt = itemView.findViewById(R.id.timeTxt);
+            priceTxt = itemView.findViewById(R.id.priceTxt);
+            pic = itemView.findViewById(R.id.pic);
+        }
+    }
+}
